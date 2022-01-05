9b77f9e196172807574a276693d21c69197045ae
Ricardo <ricardodacruz1@hotmail.com>
2022-01-05 19:36:19 -0300

    Começo

---
+++ src/main/java/com/ricardo/festa/gerenciarFesta/GerenciarFestaApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.festa.gerenciarFesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciarFestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciarFestaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/festa/gerenciarFesta/GerenciarFestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.festa.gerenciarFesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciarFestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
