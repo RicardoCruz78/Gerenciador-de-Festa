ddd7bb6c8f5ec69caf9a6d2b73b43a5b92e8c2fb
Ricardo <ricardodacruz1@hotmail.com>
2022-01-05 22:04:25 -0300

    gravando

--- src/main/java/com/ricardo/festa/gerenciarFesta/controller/ConvidadosController.java
+++ src/main/java/com/ricardo/festa/gerenciarFesta/controller/ConvidadosController.java
@@ -3,8 +3,10 @@ package com.ricardo.festa.gerenciarFesta.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.ricardo.festa.gerenciarFesta.model.Convidado;
 import com.ricardo.festa.gerenciarFesta.repository.Convidados;
 
 @Controller
@@ -17,7 +19,14 @@ public class ConvidadosController {
 	public ModelAndView listar() {
 		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
 		modelAndView.addObject("convidados", convidados.findAll());
-		return modelAndView;
+		modelAndView .addObject(new Convidado());		
+		  return modelAndView;
+	}
+	
+	@PostMapping("/convidados")
+	public String salvar(Convidado convidado) {
+		this.convidados.save(convidado);
+		return "redirect:/convidados";
 	}
 
 }
