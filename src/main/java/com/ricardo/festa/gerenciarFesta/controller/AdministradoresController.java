8fcb55920db11940555230514105bbd4abb63a9b
Ricardo <ricardodacruz1@hotmail.com>
2022-01-11 01:13:40 -0300

    metodo excluir na classe administradores

--- src/main/java/com/ricardo/festa/gerenciarFesta/controller/AdministradoresController.java
+++ src/main/java/com/ricardo/festa/gerenciarFesta/controller/AdministradoresController.java
@@ -4,6 +4,8 @@ package com.ricardo.festa.gerenciarFesta.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -27,6 +29,12 @@ public class AdministradoresController {
 	@PostMapping("/administradores" )
 	public String salvar(Administrador administrador) {
 		this.administradores.save(administrador);
+		return "redirect:/administradores" ;
+	}
+	
+	@GetMapping("/administradores/{id}/excluir")
+	public String excluir(@PathVariable Long id) {
+		administradores.deleteById(id);
 		return "redirect:/administradores";
 	}
 
--- src/main/java/com/ricardo/festa/gerenciarFesta/controller/ConvidadosController.java
+++ src/main/java/com/ricardo/festa/gerenciarFesta/controller/ConvidadosController.java
@@ -2,7 +2,6 @@ package com.ricardo.festa.gerenciarFesta.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
